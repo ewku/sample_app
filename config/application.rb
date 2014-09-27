65c30e2e6bbbe4b9c598f1de8a1fc0754478f30e
ewku <ewa.galaszewicz@gmail.com>
2014-09-26 19:42:50 -0700

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -23,6 +24,6 @@ module SampleApp
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # config.i18n.dconfig/application.rbefault_locale = :de
   end
 end
